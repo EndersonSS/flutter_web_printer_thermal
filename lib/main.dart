503b8adfd988d0a637ff00caff206669789e6c18
Enderson <eendersonserra@gmail.com>
2023-05-12 08:04:08 -0300

    init hello word

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_printer_thermal/printer_web.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web printer test',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void _printer() {
+    PrinterWeb printerWeb = PrinterWeb('Teste de impressão');
+
+    printerWeb.connectToPrinter(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'Clique aqui bb',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _printer,
+        tooltip: 'Imprimir',
+        child: const Icon(Icons.print),
+      ),
+    );
+  }
+}
---
+++ lib/printer_web.dart
@@ -0,0 +1,118 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_web_bluetooth/flutter_web_bluetooth.dart';
+import 'package:flutter_web_bluetooth/js_web_bluetooth.dart';
+
+class PrinterWeb {
+  final String data;
+
+  PrinterWeb(this.data);
+
+  late String keyService = '000018f0-0000-1000-8000-00805f9b34fb';
+  late String keyWrite = '00002af1-0000-1000-8000-00805f9b34fb';
+
+  BluetoothDevice? device;
+  List<int> uint8List = [];
+
+  connectToPrinter(BuildContext context) async {
+    uint8List = utf8.encode(data);
+
+    try {
+      final supported = FlutterWebBluetooth.instance.isBluetoothApiSupported;
+
+      if (!supported) return snackbar(context, msg: 'Navegador não é compatível', contentType: ContentType.failure);
+
+      final requestOptions = RequestOptionsBuilder.acceptAllDevices(
+          optionalServices: [keyService]);
+
+      if (device != null) {
+        device!.disconnect();
+      }
+
+      device = await FlutterWebBluetooth.instance.requestDevice(requestOptions);
+
+      await device!.gatt!.connect().then((server) {
+        snackbar(context, msg: 'Conectado a ${device!.name}, impressão em andamento...', contentType:  ContentType.success);
+        server
+            .getPrimaryService(
+              keyService,
+            )
+            .then(
+              (service) async => await _service(context,service),
+            )
+            .catchError(
+          (error) {
+            device!.disconnect();
+            print('Erro Passo 2 encontrado ao imprimir $error');
+          },
+        );
+      }).catchError(
+        (error) {
+          device!.disconnect();
+          print('Erro Passo 1 encontrado ao imprimir $error');
+        },
+      );
+    } on UserCancelledDialogError {
+      snackbar(context, msg: 'Impresão cancelada', contentType: ContentType.failure);
+    } on DeviceNotFoundError {
+      snackbar(context, msg: 
+          'Não há nenhum dispositivo ao alcance das opções definidas acima', contentType: ContentType.failure);
+    }
+  }
+
+  _service(BuildContext context, WebBluetoothRemoteGATTService service) {
+    return service
+        .getCharacteristic(
+          keyWrite,
+        )
+        .then((channel) async => await _printerToSend(context, channel))
+        .catchError(
+      (error) {
+        device!.disconnect();
+        snackbar(context, msg: 'Erro Passo 3 encontrado ao imprimir $error', contentType: ContentType.failure);
+      },
+    );
+  }
+
+  _printerToSend(BuildContext context,WebBluetoothRemoteGATTCharacteristic channel) {
+    return channel
+        .writeValueWithResponse(
+      Uint8List.fromList(uint8List),
+    )
+        .then(
+      (_) {
+        device!.disconnect();
+        print('finally');
+      },
+    ).catchError(
+      (error) {
+        print(error);
+        device!.disconnect();
+        snackbar(context, msg: 'Erro Passo 4 encontrado ao imprimir $error',contentType: ContentType.failure );
+      },
+    );
+  }
+}
+
+void snackbar(BuildContext context, {required String msg, required ContentType contentType}) {
+  final materialBanner = MaterialBanner( 
+    elevation: 0,
+    backgroundColor: Colors.transparent,
+    forceActionsBelow: true,
+    content: AwesomeSnackbarContent(
+      title: 'Opaa!!',
+      message:
+          msg,
+      contentType: contentType,
+      inMaterialBanner: true,
+    ),
+    actions: const [SizedBox.shrink()],
+  );
+
+  ScaffoldMessenger.of(context)
+    ..hideCurrentMaterialBanner()
+    ..showMaterialBanner(materialBanner);
+}
