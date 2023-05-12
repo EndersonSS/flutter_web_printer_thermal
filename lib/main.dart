aa0d4f2e00642f0be53ffff85479fd883598faf9
Enderson <eendersonserra@gmail.com>
2023-05-12 08:15:42 -0300

    build for web

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: const <Widget>[
             Text(
-              'Clique aqui bb',
+              'Teste de impressão',
             ),
           ],
         ),
